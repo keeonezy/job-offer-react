184933c3440611066ebcfb08deae34d6acfd22fc
Alexander Malukhin <keeonenew@yandex.ru>
2023-07-05 13:38:47 +0300

    refactor: google analytics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Header } from "./Header";
 import { Main } from "./Main";
 import { Footer } from "./Footer";
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga4";
 
-const TRACKING_ID = "UA-LX6NP84ZKB"; // YOUR_OWN_TRACKING_ID
+const TRACKING_ID = "G-LX6NP84ZKB"; // YOUR_OWN_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
