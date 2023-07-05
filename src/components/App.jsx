90c64cd6e86c581adecbd2591920e6e988b9151c
Alexander Malukhin <keeonenew@yandex.ru>
2023-07-05 12:40:18 +0300

    feat: add google analytics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Header } from "./Header";
 import { Main } from "./Main";
 import { Footer } from "./Footer";
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "UA-LX6NP84ZKB"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
